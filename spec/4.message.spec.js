a6ea6286614e9b73ee176ae31739cc0a878613d4
ericminio <eric.mignot@gmail.com>
2019-12-06 22:53:58 -0800

    long message

--- spec/4.message.spec.js
+++ spec/4.message.spec.js
@@ -30,4 +30,31 @@ describe('message', ()=>{
 
         expect(actual).to.deep.equal(expected)
     })
+
+    it('support long message', ()=>{
+        let input = {
+            config: {
+                padding: 2,
+                margin: 4
+            },
+            actors: [
+                { name:'superman' },
+                { name:'batman' }
+            ],
+            steps: [
+                { description:'superman -> batman : sure we can be friends'}
+            ]
+        }
+        let expected = quiet(`
+            +------------+           +----------+
+            |  superman  |           |  batman  |
+            +------------+           +----------+
+            |                        |
+            |sure we can be friends  |
+            |----------------------->|
+        `)
+        let actual = quiet(create(input))
+
+        expect(actual).to.deep.equal(expected)
+    })
 })
