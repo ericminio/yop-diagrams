83b082faba1de83d257f7010cbac96cc9bb9670f
ericminio <eric.mignot@gmail.com>
2019-12-06 20:31:44 -0800

    actor

---
+++ spec/2.actor.spec.js
@@ -0,0 +1,40 @@
+const { expect } = require('chai')
+const { create } = require('../lib')
+const { quiet } = require ('./quiet')
+
+describe('actor', ()=>{
+
+    it('is covered', ()=>{
+        let input = {
+            actors: [
+                { name:'superman' }
+            ]
+        }
+        let expected = quiet(`
+            +----------+
+            | superman |
+            +----------+
+        `)
+        let actual = quiet(create(input))
+
+        expect(actual).to.deep.equal(expected)
+    })
+    it('has default padding', ()=>{
+        let input = {
+            config: {
+                padding: 3
+            },
+            actors: [
+                { name:'superman' }
+            ]
+        }
+        let expected = quiet(`
+            +--------------+
+            |   superman   |
+            +--------------+
+        `)
+        let actual = quiet(create(input))
+
+        expect(actual).to.deep.equal(expected)
+    })
+})
