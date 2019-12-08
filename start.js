aea05605ca378dd1fff5567bb61ccd012fe6dc02
Eric Mignot <eric.mignot@gmail.com>
2019-12-08 13:30:25 -0800

    reduced noise

--- start.js
+++ start.js
@@ -2,8 +2,7 @@ const { create } = require('./lib')
 let fs = require('fs')
 let path = require('path')
 
-let input = JSON.parse(fs.readFileSync(path.join(__dirname, 'demo', process.argv[2])).toString())
-console.log(input)
+let input = JSON.parse(fs.readFileSync(path.join(__dirname, process.argv[2])).toString())
 
 let diagram = create(input)
 console.log(diagram)
