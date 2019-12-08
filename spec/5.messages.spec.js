03209a518f383df94ad9c9212868584606c27114
Eric Mignot <eric.mignot@gmail.com>
2019-12-08 12:14:05 -0800

    messages

---
+++ spec/5.messages.spec.js
@@ -0,0 +1,37 @@
+const { expect } = require('chai')
+const { create } = require('../lib')
+const { quiet } = require ('./quiet')
+
+describe('messages', ()=>{
+
+    it('work', ()=>{
+        let input = {
+            config: {
+                padding: 2,
+                margin: 4
+            },
+            actors: [
+                { name:'batman' },
+                { name:'wonder womam' }
+            ],
+            steps: [
+                { description:'batman -> wonder womam : hello'},
+                { description:'batman -> wonder womam : dinner?'}
+            ]
+        }
+        let expected = quiet(`
+            +----------+    +----------------+
+            |  batman  |    |  wonder womam  |
+            +----------+    +----------------+
+            |               |
+            |hello          |
+            |-------------->|
+            |               |
+            |dinner?        |
+            |-------------->|
+        `)
+        let actual = quiet(create(input))
+
+        expect(actual).to.deep.equal(expected)
+    })
+})
