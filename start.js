ec2c053263c2eb891b7620ef04ae3f47707b48c1
Eric Mignot <eric.mignot@gmail.com>
2019-12-09 09:40:39 -0800

    ready for npm publish

--- start.js
+++ start.js
@@ -3,6 +3,6 @@ let fs = require('fs')
 let path = require('path')
 
 let input = JSON.parse(fs.readFileSync(path.join(__dirname, process.argv[2])).toString())
-
 let diagram = create(input)
+
 console.log(diagram)
