3cb0a45aebd57f819791868213ec47df7fcc88df
ericminio <eric.mignot@gmail.com>
2021-02-05 21:59:00 -0800

    vertical link

--- spec/components/3.link.spec.js
+++ spec/components/3.link.spec.js
@@ -20,7 +20,35 @@ describe('link', ()=>{
             +--------+     +--------+
             | One    |-----| Two    |
             |        |     |        |
-            +--------+     +--------+                              
+            +--------+     +--------+
+        `)
+        let actual = quiet(create(input))
+
+        expect(actual).to.deep.equal(expected)
+    })
+    it('can be vertical', ()=>{
+        let input = {
+            components: [
+                { name:'One', x:0, y:0, width:10, height:4 },
+                { name:'Two', x:6, y:0, width:10, height:4 }
+            ],
+            links: [
+                { name:'one-two', path: [
+                    { x:4, y:2 }, { x:5, y:2 }
+                ] }
+            ]
+        }
+        let expected = quiet(`
+            +--------+ 
+            | One    |
+            |        |     
+            +--------+
+              |
+              |
+            +--------+ 
+            | Two    |
+            |        |     
+            +--------+
         `)
         let actual = quiet(create(input))
 
