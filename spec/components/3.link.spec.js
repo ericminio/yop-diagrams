d33d7742882191a31449318797f9b494c0e7dddb
ericminio <eric.mignot@gmail.com>
2021-02-06 13:16:33 -0800

    left-up link

--- spec/components/3.link.spec.js
+++ spec/components/3.link.spec.js
@@ -82,4 +82,32 @@ describe('link', ()=>{
 
         expect(actual).to.deep.equal(expected)
     })
+    it('can go left and up', ()=>{
+        let input = {
+            components: [
+                { name:'One', row:0, column:0, width:10, height:4 },
+                { name:'Two', row:6, column:10, width:10, height:4 }
+            ],
+            links: [
+                { origin:'Two', path: [
+                    { row:1, column:-1 }, { row:1, column:-8 }, { row:-2, column:-8 }
+                ] }
+            ]
+        }
+        let expected = quiet(`
+            +--------+ 
+            | One    |
+            |        |     
+            +--------+
+              |
+              |
+              |       +--------+ 
+              +-------| Two    |
+                      |        |     
+                      +--------+
+        `)
+        let actual = quiet(create(input))
+
+        expect(actual).to.deep.equal(expected)
+    })
 })
